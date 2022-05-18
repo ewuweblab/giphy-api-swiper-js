72b27c7c55331a0e1a05988fb924d4a2edd6533e
EWU Web.Lab <pmanikoth@ewu.edu>
2022-05-18 09:17:13 -0700

    Debug : NO PWA

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-// Set Template
+// initialize Swiper
 const swiper = new Swiper(".swiper", {});
 
-/*** Variables ***/
+// VARIABLES
 // API Key
 const API_KEY = 'vRgpxNRNKxyIlc1sk9lEifd6kDJZDK57';
-// limit
-const limit = 25; 
-// Get `input`
-const inputField = document.querySelector('.search-input');
-// Initialize or Default search query
-inputField.value = '';
+// Search term
+searchTerm = 'coffee';
 
-// Listen to key presses
-inputField.addEventListener('keyup', event => {
-  
-  if (event.key === 'Enter') {
-    
-    // Go fetch Giphy API data
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${inputField.value}&limit=${limit}`)
+// Go fetch Giphy API data
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=10`)
       .then( response => response.json() )
       .then( gifs => {
-     
-      // check-check
-      console.log(gifs);
-      console.log(gifs.data);
+      
+        // Check Check         
+        console.log(gifs.data);
+        console.log(gifs.data[2].images.original.mp4);
       
       // Get container for data
-      const videoContainer = document.querySelector('.swiper-wrapper');
+      const videoContainer = document.querySelector('.swiper-wrapper');         
       
-      // Loop through the array of data
+       // Loop through the array of data
       gifs.data.forEach( gif => {
         
           // template 
           const template  = `
-          <div class="swiper-slide">
-            <video src="${gif.images.original.mp4}" autoplay loop></video>
-          </div>
+            <video class="swiper-slide" src="${gif.images.original.mp4}" autoplay loop></video>
           `;
         
           // append
@@ -43,14 +32,4 @@ inputField.addEventListener('keyup', event => {
         
       });
       
-      
     });
-    
-    // Reset value and return cursor focus -- of input field
-    inputField.value = '';
-    inputField.focus();
-    
-  }
-  
-});
-
