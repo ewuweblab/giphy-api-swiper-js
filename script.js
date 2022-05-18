27a36db4a19ac88be180fe7438dd0bfff6bccca2
EWU Web.Lab <pmanikoth@ewu.edu>
2022-05-18 09:30:34 -0700

    Trending API

--- script.js
+++ script.js
@@ -4,21 +4,20 @@ const swiper = new Swiper(".swiper", {});
 // VARIABLES
 // API Key
 const API_KEY = 'vRgpxNRNKxyIlc1sk9lEifd6kDJZDK57';
-// Search term
-searchTerm = 'coffee';
+// Trending URL
+const url = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`
 
 // Go fetch Giphy API data
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=10`)
+    fetch(url)
       .then( response => response.json() )
       .then( gifs => {
       
-        // Check Check         
-        console.log(gifs.data);
-        console.log(gifs.data[2].images.original.mp4);
+        // Check-Check data
+        //console.log(gifs.data);
+        //console.log(gifs.data[0].images.original.mp4);
       
       // Get container for data
       const videoContainer = document.querySelector('.swiper-wrapper');
-      const video = document.querySelector("video");      
       
        // Loop through the array of data
       gifs.data.forEach( gif => {
@@ -32,12 +31,6 @@ searchTerm = 'coffee';
           videoContainer.insertAdjacentHTML("afterbegin", template);
         
       });
-      
-      
-    swiper.on('click', function () {
-      video.play();
-    });
-      
 
     });
 
