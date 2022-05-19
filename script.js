74d8e8b23998546a661d0515bd9b9a307c79a166
EWU Web.Lab <pmanikoth@ewu.edu>
2022-05-18 20:18:27 -0700

    Video to Images

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 // initialize Swiper
 const swiper = new Swiper(".swiper", {});
 
+// VARIABLES
+// API Key
+const API_KEY = 'vRgpxNRNKxyIlc1sk9lEifd6kDJZDK57';
+// Search term
+searchTerm = 'coffee';
+
 // Go fetch Giphy API data
-    fetch('https://api.giphy.com/v1/gifs/trending?api_key=vRgpxNRNKxyIlc1sk9lEifd6kDJZDK57&limit=50&rating=r')
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=vRgpxNRNKxyIlc1sk9lEifd6kDJZDK57&limit=25&rating=r')
       .then( response => response.json() )
       .then( gifs => {
       
-        // Check-Check data
+        // Check Check         
         //console.log(gifs.data);
-        //console.log(gifs.data[0].images.original.mp4);
+        //console.log(gifs.data[0].images.downsized.url);
       
       // Get container for data
-      const videoContainer = document.querySelector('.swiper-wrapper');
+      const videoContainer = document.querySelector('.swiper-wrapper');         
       
        // Loop through the array of data
       gifs.data.forEach( gif => {
         
           // template 
           const template  = `
-            <video class="swiper-slide" src="${gif.images.original.mp4}" autoplay loop></video>
+            <div class="swiper-slide">
+              <img src="${gif.images.downsized.url}" />
+            </div>
           `;
         
           // append
           videoContainer.insertAdjacentHTML("afterbegin", template);
         
       });
-
+      
     });
-
